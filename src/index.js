5cd6e47257e8de57a107deb3b5b707c4f28aa990
ViridianFairy <297091829@qq.com>
2020-02-23 21:17:04 +0800

    可以返回到主省图了

--- src/index.js
+++ src/index.js
@@ -17,46 +17,56 @@ window.jump = function (name) {
 function randomValue() {
    return 0
 }
-function getFullName(name){
-   var obj = dataList.find(val=>{
+function getFullName(name) {
+   var obj = dataList.find(val => {
       return val.name == name
    })
    return obj.nameFull
 }
+function back(){
+   Main.style.display = 'none'
+   Loading.style.display="block"
+   myChart.dispose()
+   
+   dataList = []
+   dataList2 = []
+   dataTime = []
+   begin()
+}
 var dataList = [
-   { name: '南海诸岛',nameFull: '南海诸岛', value: -1 },
-   { name: '北京',nameFull: '北京市', value: randomValue() },
-   { name: '天津',nameFull: '天津市', value: randomValue() },
-   { name: '上海',nameFull: '上海市', value: randomValue() },
-   { name: '重庆',nameFull: '重庆市', value: randomValue() },
-   { name: '河北',nameFull: '河北省', value: randomValue() },
-   { name: '河南',nameFull: '河南省', value: randomValue() },
-   { name: '云南',nameFull: '云南省', value: randomValue() },
-   { name: '辽宁',nameFull: '辽宁省', value: randomValue() },
-   { name: '黑龙江',nameFull: '黑龙江省', value: randomValue() },
-   { name: '湖南',nameFull: '湖南省', value: randomValue() },
-   { name: '安徽',nameFull: '安徽省', value: randomValue() },
-   { name: '山东',nameFull: '山东省', value: 5555 },
-   { name: '新疆',nameFull: '新疆维吾尔自治区', value: 99999 },
-   { name: '江苏',nameFull: '江苏省', value: randomValue() },
-   { name: '浙江',nameFull: '浙江省', value: randomValue() },
-   { name: '江西',nameFull: '江西省', value: 555 },
-   { name: '湖北',nameFull: '湖北省', value: randomValue() },
-   { name: '广西',nameFull: '广西壮族自治区', value: randomValue() },
-   { name: '甘肃',nameFull: '甘肃省', value: randomValue() },
-   { name: '山西',nameFull: '山西省', value: randomValue() },
-   { name: '内蒙古',nameFull: '内蒙古自治区', value: randomValue() },
-   { name: '陕西',nameFull: '陕西省', value: randomValue() },
-   { name: '吉林',nameFull: '吉林省', value: randomValue() },
-   { name: '福建',nameFull: '福建省', value: randomValue() },
-   { name: '贵州',nameFull: '贵州省', value: randomValue() },
-   { name: '广东',nameFull: '广东省', value: randomValue() },
-   { name: '青海',nameFull: '青海省', value: randomValue() },
-   { name: '西藏',nameFull: '西藏自治区', value: randomValue() },
-   { name: '四川',nameFull: '四川省', value: randomValue() },
-   { name: '宁夏',nameFull: '宁夏回族自治区', value: randomValue() },
-   { name: '海南',nameFull: '海南省', value: randomValue() },
-   { name: '台湾',nameFull: '台湾省', value: randomValue() },
+   { name: '南海诸岛', nameFull: '南海诸岛', value: -1 },
+   { name: '北京', nameFull: '北京市', value: randomValue() },
+   { name: '天津', nameFull: '天津市', value: randomValue() },
+   { name: '上海', nameFull: '上海市', value: randomValue() },
+   { name: '重庆', nameFull: '重庆市', value: randomValue() },
+   { name: '河北', nameFull: '河北省', value: randomValue() },
+   { name: '河南', nameFull: '河南省', value: randomValue() },
+   { name: '云南', nameFull: '云南省', value: randomValue() },
+   { name: '辽宁', nameFull: '辽宁省', value: randomValue() },
+   { name: '黑龙江', nameFull: '黑龙江省', value: randomValue() },
+   { name: '湖南', nameFull: '湖南省', value: randomValue() },
+   { name: '安徽', nameFull: '安徽省', value: randomValue() },
+   { name: '山东', nameFull: '山东省', value: 5555 },
+   { name: '新疆', nameFull: '新疆维吾尔自治区', value: 99999 },
+   { name: '江苏', nameFull: '江苏省', value: randomValue() },
+   { name: '浙江', nameFull: '浙江省', value: randomValue() },
+   { name: '江西', nameFull: '江西省', value: 555 },
+   { name: '湖北', nameFull: '湖北省', value: randomValue() },
+   { name: '广西', nameFull: '广西壮族自治区', value: randomValue() },
+   { name: '甘肃', nameFull: '甘肃省', value: randomValue() },
+   { name: '山西', nameFull: '山西省', value: randomValue() },
+   { name: '内蒙古', nameFull: '内蒙古自治区', value: randomValue() },
+   { name: '陕西', nameFull: '陕西省', value: randomValue() },
+   { name: '吉林', nameFull: '吉林省', value: randomValue() },
+   { name: '福建', nameFull: '福建省', value: randomValue() },
+   { name: '贵州', nameFull: '贵州省', value: randomValue() },
+   { name: '广东', nameFull: '广东省', value: randomValue() },
+   { name: '青海', nameFull: '青海省', value: randomValue() },
+   { name: '西藏', nameFull: '西藏自治区', value: randomValue() },
+   { name: '四川', nameFull: '四川省', value: randomValue() },
+   { name: '宁夏', nameFull: '宁夏回族自治区', value: randomValue() },
+   { name: '海南', nameFull: '海南省', value: randomValue() },
+   { name: '台湾', nameFull: '台湾省', value: randomValue() },
    { name: '香港', vnameFull: '香港', value: randomValue() },
    { name: '澳门', vnameFull: '澳门', value: randomValue() }
 ]
@@ -141,41 +151,50 @@ myChart.on("mouseover", function (params) {
 var dataList2 = []
 var dataTime = []
 function make(query) {
-   console.log(getFullName(query))
    myChart.dispose()
    $ajax.get(`https://lab.isaaclin.cn/nCoV/api/area?latest=0&province=${getFullName(query)}`)
       .then(doc => {
-         console.dir(doc)
-         
-         doc.data.results.forEach(v=>{
+         doc.data.results.forEach(v => {
             let t = new Date(v.updateTime)
-            let time = `${t.getMonth()+1}.${t.getDate()}`
-            if(dataTime.includes(time)) return
+            let time = `${t.getMonth() + 1}.${t.getDate()}`
+            if (dataTime.includes(time)) return
             dataTime.unshift(time)
             dataList2.unshift(
                v.confirmedCount,
             )
          })
-   myChart = echarts.init(document.getElementById('main'))
-   myChart.setOption({
-      xAxis: {
-         type: 'category',
-         data: dataTime,
-         name:'时间'
-     },
-     yAxis: {
-         type: 'value',
-         name:getFullName(query)+'的确诊人数啊啊'
-     },
-     series: [{
-         data: dataList2,
-         type: 'line',
-         smooth: true
-     }]
-   });
+         myChart = echarts.init(document.getElementById('main'))
+         myChart.setOption({
+            xAxis: {
+               type: 'category',
+               data: dataTime,
+               name: '时间'
+            },
+            yAxis: {
+               type: 'value',
+               name: getFullName(query) + '的确诊人数啊啊'
+            },
+            series: [{
+               data: dataList2,
+               type: 'line',
+               smooth: true
+            }],
+            tooltip: {
+               //鼠标悬停提示内容
+               trigger: 'axis', // 触发类型，默认数据触发，可选为：'axis' item
+               axisPointer: {
+                   // 坐标轴指示器，坐标轴触发有效
+                   type: "line", // 默认为直线，可选为：'line' | 'shadow'
+                   label:'cross',
+                   show:true
+                 }, 
+               }
+         });
+         myChart.dispatchAction({
+            type: 'showTip',
+            seriesIndex:1,  // 显示第几个series
+         });
       })
-   
-   
 }
 $ajax.get('https://lab.isaaclin.cn/nCoV/api/area')
    .then(doc => {
@@ -186,7 +205,11 @@ $ajax.get('https://lab.isaaclin.cn/nCoV/api/area')
             }
          })
       })
-      myChart.setOption(option);
-      Loading.style.display = 'none'
-      Main.style.display = 'block'
+      begin()
    })
+function begin(){
+   myChart.setOption(option);
+   Loading.style.display = 'none'
+   Main.style.opacity = '1'
+   Main.style.display = 'block'
+}
